797b9c40227c85c4ff216eec98b22c36e0952984
jtlocati <jetlocati4922@gmail.com>
2025-07-19 13:23:28 -0600

    fixing of functions within DB.py
    addition of 'close_connection' def for flask users (include in documentation), fixing of the return statements within _INITALIZATION def's and cursor keyword, all _INITALIZATION statements can now be ran with no parameters beyond filling out the GET_WORKBENCH_INFO, and get_DATABASE_NAME.

--- Fundamental_Query_Language/FQL_core/DB.py
+++ Fundamental_Query_Language/FQL_core/DB.py
@@ -2,24 +2,26 @@ import mysql.connector
 from flask import g
 import sqlite3
 
+WORKBENCH_HOST =""
+WORKBENCH_USERNAME = ""
+WORKBENCH_PASS = ""
+
 #holds name for SQL database
-def DB_NAME(DataBase_Name):
-    return DataBase_Name    
-    
-    
+def get_DB_NAME(DataBase_Name):
+    return DataBase_Name   
+
+
 #shows what orentation of db this is
 def DB_LOCATION(Location):
     if Location.upper() == "LOCAL":
         return "LOCAL"
     elif Location.uppper == "WORKBENCH":
         return "WORKBENCH"
-    elif(Location.upper() == "FLASK"):
-        return "FLASK"
     else:
         return ValueError(f"----ERROR---- The command: 'DB.BDLOCATION({Location})' cannot be intrepreted.\n current values are: 'LOCAL' or 'WOEKBENCH'")
     
 
-#weather the user is using an SQL or SQLite application
+#weather the user is using an SQL or SQLite application, to be used in hardcode
 def SQL_TYPE(Type):
     if Type.upper() == "SQLite":
         return"lite"
@@ -27,36 +29,66 @@ def SQL_TYPE(Type):
 
         return ValueError(f"----ERROR----\n THE VALUE OF 'DB.SQL_TYPE({type})' CANNOT BE IDENTIFYED\n MUST BE OF VALUE 'SQL'  OR 'SQLite'")
     
+def ISFLASK(Type):
+    if Type.upper() == "FLASK":
+        return True
+    elif Type.upper() == "OTHER":
+        return False
+    
     
 #TIE THE FOLLOWIING TOW DEFS INTO LOCATION DEF -> CHECK IF POSSIBLE
 # _LOCAL AND _WORKBENCH INITTALIZATION OF DB'S
-def DB_INITALIZATION_LOCAL(DATA_BASE):
+#figureout a way to pass the get_DB_NAME as the DATABASE variable
+#is equivelent to get_DB
+
+def GET_WORKBENCH_INFO(HOST, USER_NAME, PASSWORD):
+    WORKBENCH_HOST = HOST
+    WORKBENCH_USERNAME = USER_NAME
+    WORKBENCH_PASS = PASSWORD
+
+def DB_INITALIZATION_LOCAL(DATABASE = get_DB_NAME):
+
     if DB_LOCATION != "LOCAL":
         return ValueError(f"----ERROR----\nThe command: 'DB_INITALIZATION_WORKBENCH()' is not needed fot the data type of: {DB_LOCATION}")
     
-    else:
-        conn = sqlite3.connect(DATA_BASE)
+    elif DB_LOCATION == "LOCAL" and ISFLASK == False:
+        conn = sqlite3.connect(DATABASE)
+        return conn.cursor()
     
+    elif DB_LOCATION == "LOCAL" and ISFLASK == True:
+        if 'db' not in g:
+            g.db = sqlite3.connect(DATABASE)
+        db = g.db
+        return db.cursor()
+    else:
+        ValueError(f"----ERROR----\nThe input in method:'DB_INITALIZATION_LOCAL({DATABASE})' is invalid\n check your values and please ensure that the 'ISFLASK()' method is filled out properly")
 
-def DB_INFO_WORKBENCH(HOST, USER_NAME, PASSWORD):
+def DB_INITALIZATION_WORKBENCH():
     if DB_LOCATION == "LOCAL":
         return ValueError(f"----ERROR----\nThe command: 'DB_INITALIZATION_WORKBENCH()' is not needed fot the data type of: {DB_LOCATION}")
     
-    elif DB_LOCATION == "WORKBENCH":
+    elif DB_LOCATION == "WORKBENCH" and ISFLASK == False:
         db = mysql.connector.connect(
-            host = HOST,
-            user = USER_NAME,
-            password = PASSWORD,
-            database = DB_NAME
+            host = WORKBENCH_HOST,
+            user = WORKBENCH_USERNAME,
+            password = WORKBENCH_PASS,
+            database = get_DB_NAME
         )
         cursor = db.cursor()
 
-    elif DB_LOCATION == "FLASK":
+    elif DB_LOCATION == "WORKBENCH" and ISFLASK == True:
         if 'db' not in g:
             g.db = mysql.connector.connect(
-                host = HOST,
-                user = USER_NAME,
-                password = PASSWORD,
-                database = DB_NAME
+                host = WORKBENCH_HOST,
+                user = WORKBENCH_USERNAME,
+                password = WORKBENCH_PASS,
+                database = get_DB_NAME
             )
         return g.db
+    else:
+        ValueError(f"----ERROR----\nThe input in method:'DB_INFO_WORKBENCH()' is invalid\n check your values within: 'GET_WORKBENCH_INFO'and please ensure that the 'ISFLASK()' method is filled out properly")
+
+def close_connection(exception):
+    db = g.pop('db', None)
+    if db:
+        db.close()
